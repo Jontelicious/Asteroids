9efab77837debe0dfe809ab831528775a5577397
Jontelicious <250360915+Jontelicious@users.noreply.github.com>
2026-01-02 21:29:56 +0100

    Asteroids!

---
+++ main.py
@@ -0,0 +1,26 @@
+from constants import SCREEN_WIDTH
+from constants import SCREEN_HEIGHT
+from logger import log_state
+import pygame
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    VERSION = pygame.version.ver
+    print(f"Starting Asteroids with pygame version: {VERSION} ")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    while True:
+        log_state()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        pygame.Surface.fill(screen, (0, 0, 0))
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
