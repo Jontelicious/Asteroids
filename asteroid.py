f23ae93d40cb524e7fb924c4bae1a681f811194f
Jontelicious <250360915+Jontelicious@users.noreply.github.com>
2026-01-03 16:44:07 +0100

    final update?

--- asteroid.py
+++ asteroid.py
@@ -2,6 +2,8 @@ import pygame
 from circleshape import *
 from player import *
 from constants import *
+import random
+from logger import log_event
 
 
 class Asteroid(CircleShape):
@@ -13,3 +15,20 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        else:
+            log_event("asteroid_split")
+            random_angle = random.uniform(20, 50)
+            vector1 = self.velocity.rotate(random_angle)
+            vector2 = self.velocity.rotate(-random_angle)
+
+            new_radius = self.radius - ASTEROID_MIN_RADIUS
+            asteroid1 = Asteroid(self.position.x, self.position.y, new_radius)
+            asteroid1.velocity = vector1 * 1.2
+            asteroid2 = Asteroid(self.position.x, self.position.y, new_radius)
+            asteroid2.velocity = vector2 * 1.2
--- main.py
+++ main.py
@@ -44,6 +44,13 @@ def main():
         updatable.update(dt)
 
         for asteroid in asteroids:
+            for shot in shots:
+                if shot.collides_with(asteroid):
+                    log_event("asteroid_shot")
+                    asteroid.split()
+                    shot.kill()
+
+        for asteroid in asteroids:
             if player.collides_with(asteroid):
                 log_event("player_hit")
                 print("Game Over!")
